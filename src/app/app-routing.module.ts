df4a1d61d279f4e56b333751e35ee57e29b4f18b
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-08 12:31:50 +0300

    [feat] add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule }                                from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard }                               from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -13,7 +14,8 @@ const routes: Routes = [
   },
   {
     path: 'shopping-list/add',
-    loadChildren: () => import('./pages/shopping-list/create-item/create-item.module').then( m => m.CreateItemPageModule)
+    loadChildren: () => import('./pages/shopping-list/create-item/create-item.module').then( m => m.CreateItemPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
