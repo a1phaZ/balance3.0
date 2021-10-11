1dd7540311da530f7bc2b92fbca67c35455f718a
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-11 12:07:36 +0300

    [feat] pin-pad in component

---
+++ src/app/pages/user/pin-pad/keyboard/keyboard.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router }                                  from '@angular/router';
+
+@Component({
+  selector: 'app-keyboard',
+  templateUrl: './keyboard.component.html',
+  styleUrls: ['./keyboard.component.scss'],
+})
+export class KeyboardComponent implements OnInit {
+  @Output() fillDots = new EventEmitter();
+  savedPin: number[] = [];
+  pin: number[] = [];
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.savedPin = JSON.parse(localStorage.getItem('pin')) || [];
+  }
+
+  onClick(digit = -1) {
+    if (digit === -1) {
+      localStorage.setItem('pin', JSON.stringify([]));
+      localStorage.setItem('user', JSON.stringify(null));
+      this.router.navigate(['/']);
+      console.log('-1');
+      return;
+    }
+    this.pin = [...this.pin, digit];
+    this.fillDots.emit({length: this.pin.length, error: null});
+    if (this.pin.length === 5) {
+      if (this.comparePin(this.savedPin, this.pin)) {
+        this.router.navigate(['shopping-list/add']);
+      } else {
+        this.pin = [];
+        this.fillDots.emit({error: 'Pin error', length: 0});
+      }
+    }
+  }
+
+  comparePin(savedPin, pin): boolean {
+    if (savedPin.length === 0) {
+      localStorage.setItem('pin', JSON.stringify(pin));
+      return true;
+    }
+    return !!savedPin.every((num, idx) => num === pin[idx]);
+  }
+
+}
