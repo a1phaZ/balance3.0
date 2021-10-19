7653a42682f14bb0b3e6267f95822c12a04651a3
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-19 17:51:57 +0300

    [feat] get cards & get modal data

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }      from '@angular/core';
+import SwiperCore, { Pagination } from 'swiper';
+import { CardService, ICard }     from '../../services/card.service';
+import { ModalPage }              from '../modal/modal.page';
+import { ModalService }           from '../../services/modal.service';
+
+SwiperCore.use([Pagination]);
 
 @Component({
   selector: 'app-main',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.page.scss'],
 })
 export class MainPage implements OnInit {
+  cards: ICard[];
 
-  constructor() { }
+  constructor(
+    private cardService: CardService,
+    private modalCtrl: ModalService
+  ) {
+    modalCtrl.modalData.subscribe((res) => {
+      console.log(res);
+    });
+  }
 
   ngOnInit() {
+    this.cardService.getAll().subscribe(data => this.cards = data);
+  }
+
+  async addCardClick() {
+    // TODO Собирать данные из ввода
+    const params = {
+      fields
+    };
+    await this.modalCtrl.openModal(ModalPage, params);
   }
 
 }
+
+const fields = [
+  {
+    type: 'text',
+    name: 'title'
+  },
+  {
+    type: 'number',
+    name: 'balance',
+  },
+];
