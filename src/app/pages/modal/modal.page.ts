f7c84c8d4a2b74db2d09cf17c2cd6df9ac07c4db
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-19 17:49:45 +0300

    [feat] base modal page

--- src/app/pages/modal/modal.page.ts
+++ src/app/pages/modal/modal.page.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import Purchase                   from '../../models/purchase';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { ModalService }             from '../../services/modal.service';
 
 @Component({
   selector: 'app-modal',
@@ -9,28 +8,20 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ModalPage implements OnInit {
   @Input() item: any;
+  @Input() fields: any[];
+  data: any;
 
-  form: FormGroup;
+  // form: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private modalCtrl: ModalService) {
+  }
 
   ngOnInit() {
-    const purchase = new Purchase();
-    this.form = this.fb.group(purchase);
-    this.form.patchValue({name: this.item.name});
-    console.log(this.form.controls, this.form.value);
+    console.log(this.fields);
+    this.data = this.fields;
   }
 
-
-  calc() {
-    console.log(this.form.value);
-    const {count = 0, price = 0} = this.form.value;
-    console.log(count, price, count * price);
-    this.form.patchValue({sum: count * price});
-  }
-
-  onSubmit(event) {
-    event.preventDefault();
-    console.log(this.form.value);
+  dismissModal() {
+    this.modalCtrl.dismissModal(this.data);
   }
 }
--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable }      from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalPage }       from '../pages/modal/modal.page';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+  modalData: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
+
+  async openModal(modalPage: typeof ModalPage, params) {
+    const modal = await this.modalCtrl.create({
+      component: modalPage,
+      componentProps: {...params}
+    });
+    modal.onDidDismiss()
+      .then((modalData) => {
+        this.modalData.next(modalData.data);
+      });
+
+    await modal.present();
+  }
+
+  async dismissModal(data) {
+    await this.modalCtrl.dismiss(data);
+  }
 }
