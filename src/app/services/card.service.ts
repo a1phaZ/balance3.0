161a4f874ec0bc55797ca0d80058182022eba972
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-21 15:31:28 +0300

    [feat] get & save cards

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,27 +1,44 @@
-import { Injectable }      from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable }                                   from '@angular/core';
+import { BehaviorSubject }                              from 'rxjs';
+import { map }                                          from 'rxjs/internal/operators';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { AuthenticationService }                        from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardService {
   cards: BehaviorSubject<ICard[]> = new BehaviorSubject<ICard[]>([]);
-  constructor() { }
+  cardsListRef: AngularFirestoreCollection<ICard> = null;
+  private dbPath = '/cards';
+
+  constructor(
+    private db: AngularFirestore,
+    private auth: AuthenticationService,
+  ) {
+    this.cardsListRef = db.collection(this.dbPath, ref => ref.where('userId', '==', auth.userId).orderBy('date'));
+  }
 
   getAll() {
-    return this.cards;
+    return this.cardsListRef;
   }
 
   addCard(data: ICard) {
-    const _cards = this.cards.getValue();
-    _cards.push(data);
-    this.cards.next(_cards);
+    return this.cardsListRef.add({...data, userId: this.auth.userId});
+  }
+
+  getCard(id) {
+    return this.cards.pipe(
+      map((cards) => cards.find((card) => card.id === id)));
   }
 }
 
 export interface ICard {
+  id: string;
+  userId: string;
   title: string;
   balance: number;
   operation: any[];
   icon: string;
+  date: number;
 }
