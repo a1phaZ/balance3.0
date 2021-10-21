6902e29a4992bad5266bfcea6b307e4f9b420354
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-21 17:38:22 +0300

    [feat] add option on income field

--- src/app/services/fields.service.ts
+++ src/app/services/fields.service.ts
@@ -46,15 +46,11 @@ export class FieldsService {
     return {
       type: 'transactions',
       fields: [
-        {
-          type: 'text',
-          name: 'title',
-          title: 'Наименование',
-          validators: [Validators.required]
-        },
+        {name: 'title', type: 'text', title: 'Наименование', validators: [Validators.required]},
         {name: 'price', type: 'number', title: 'Цена', validators: [Validators.required]},
         {name: 'count', type: 'number', title: 'Количество', validators: [Validators.required]},
-        {name: 'income', type: 'radio', title: 'Доход/Расход', validators: [Validators.required]},
+        // eslint-disable-next-line max-len
+        {name: 'income', type: 'select', options: [{id: true, title: 'Доход'}, {id: false, title: 'Расход'}], title: 'Доход/Расход', validators: [Validators.required]},
         {name: 'tags', type: 'text', title: 'Категории', validators: []}
       ]
     };
