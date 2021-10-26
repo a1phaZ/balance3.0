cf353f2ff2ea88483309c01818c3e8a524e816f0
Artemiy Zebzeev <a.zebzeev@keyauto.ru>
2021-10-26 17:30:57 +0300

    [fix] non data object in props

--- src/app/shared/handlers.ts
+++ src/app/shared/handlers.ts
@@ -1,2 +1,6 @@
 // eslint-disable-next-line max-len
-export const compareDate = (d1, d2) => (d1.getDate() === d2.getDate() && d1.getMonth() === d2.getMonth() && d1.getFullYear() === d2.getFullYear());
+export const compareDate = (d1, d2) => {
+  const _d1 = new Date(d1);
+  const _d2 = new Date(d2);
+  return _d1.getDate() === _d2.getDate() && _d1.getMonth() === _d2.getMonth() && _d1.getFullYear() === _d2.getFullYear();
+};
